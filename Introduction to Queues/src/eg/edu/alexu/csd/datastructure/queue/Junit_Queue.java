aefc3a0641c9064aff4ceb12dda08204be9f29d7
ahmed <ahmed@192.168.1.7>
2020-04-07 12:09:31 +0200

    initialize files

---
+++ Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/Junit_Queue.java
@@ -0,0 +1,14 @@
+package eg.edu.alexu.csd.datastructure.queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Junit_Queue {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/LinkedBasedQueue.java
@@ -0,0 +1,29 @@
+package eg.edu.alexu.csd.datastructure.queue;
+
+public class LinkedBasedQueue implements ILinkedBased {
+
+	@Override
+	public void enqueue(Object item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
