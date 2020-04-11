0413e2c5bf26dce90f26398b72087344810e48ac
ahmed <ahmed@192.168.1.7>
2020-04-11 23:39:30 +0200

    linked based queue

--- Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/LinkedBasedQueue.java
+++ Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/LinkedBasedQueue.java
@@ -2,28 +2,54 @@ package eg.edu.alexu.csd.datastructure.queue;
 
 public class LinkedBasedQueue implements ILinkedBased {
 
+	private class Node {
+		Object element;
+		Node next;
+
+		private Node(Object item) {
+			this.element = item;
+			this.next = null;
+		}
+	}
+	private Node head, tail;
+	private int size;
+	private LinkedBasedQueue() {
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
+
 	@Override
 	public void enqueue(Object item) {
-		// TODO Auto-generated method stub
-		
+		Node extra = new Node(item);
+		if (isEmpty()) {
+			head = tail = extra;
+		} else {
+			tail.next = extra;
+			tail = tail.next;
+		}
+		size++;
 	}
 
 	@Override
 	public Object dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+			throw new RuntimeException("the queue is empty");
+		}
+		Object temp = this.head.element;
+		this.head = this.head.next;
+		this.size--;
+		return temp;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.head == null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
 }
