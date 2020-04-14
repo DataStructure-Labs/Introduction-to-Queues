b0ec89b55f78f3936fefc049ef2d8e358213b3cb
Ahmed <Ahmed@My-HP>
2020-04-14 15:51:12 +0200

    testing eclipse commit and pull

--- Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/ArrayBasedQueue.java
+++ Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/ArrayBasedQueue.java
@@ -4,25 +4,24 @@ public class ArrayBasedQueue implements IArrayBased{
 
 	@Override
 	public void enqueue(Object item) {
-		// TODO Auto-generated method stub
-		
+		// testing Eclipse commit
 	}
 
 	@Override
 	public Object dequeue() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
