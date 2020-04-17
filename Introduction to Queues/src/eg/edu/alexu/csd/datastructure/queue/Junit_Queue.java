96ce0b3f70e071500c5a67bcc13159f9b61efbc0
ahmed <ahmed@192.168.1.7>
2020-04-17 21:56:45 +0200

    Junit test

--- Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/Junit_Queue.java
+++ Introduction to Queues/src/eg/edu/alexu/csd/datastructure/queue/Junit_Queue.java
@@ -33,12 +33,50 @@ class Junit_Queue {
 		l.enqueue("Radwan '_'  ");
 		
 		assertEquals("Radwan '_'  ",l.dequeue());
-		//////////////////////////////////////////
+		
+		//check throw empty queue
 		assertThrows(RuntimeException.class,()->{l.dequeue();});
 	}
 	
 	@Test
 	void ArrayBasedTest() {
+		ArrayBasedQueue l = new ArrayBasedQueue(7) ;
+		l.enqueue('a');
+		l.enqueue(17);
+		l.enqueue(18);
+		l.enqueue("ahmed");
+		l.enqueue(2000);
+		l.enqueue("khaled");
+		assertEquals(6,l.size());
+		assertEquals('a',l.dequeue());
+		l.enqueue("fawzy");
+		l.enqueue("ibrahim");
+		assertEquals(17,l.dequeue());
+		assertEquals(18,l.dequeue());
+		assertEquals(5,l.size());
+		assertEquals("ahmed",l.dequeue());
+		assertEquals(2000,l.dequeue());
+		assertEquals("khaled",l.dequeue());
+		assertEquals("fawzy",l.dequeue());
+		assertEquals("ibrahim",l.dequeue());
+		
+		assertEquals(0,l.size());
+		
+		l.enqueue("Radwan '_'  ");
+		
+		assertEquals("Radwan '_'  ",l.dequeue());
+		// check throw empty queue
+		assertThrows(RuntimeException.class,()->{l.dequeue();});
+		
+		//check throw full queue
+		l.enqueue('a');
+		l.enqueue(17);
+		l.enqueue(18);
+		l.enqueue("ahmed");
+		l.enqueue(2000);
+		l.enqueue("khaled");
+		l.enqueue("Yousef");
+		assertThrows(RuntimeException.class,()->{l.enqueue("throw");});
 		
 	}
 
